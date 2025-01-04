378860c85d6aa1c57161411ec6629ea8c779708d
Jozo Krstanović <jozo.molten@gmail.com>
2025-01-04 21:05:58 +0100

    Introduce dynamic routes to the web app. Implement jsonplaceholder data fetching.

---
+++ src/app/community-motivation/forums/[id]/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { BASE_API_URL } from "../page";
+import type { Post } from "../page";
+
+type BlogPostProps = {
+  params: { id: string };
+};
+  
+async function getPost(id: string): Promise<Post> {
+	const data = await fetch(`${BASE_API_URL}/posts/${id}`);
+	return data.json();
+}
+
+//This is a placeholder component to showcase dynamic routes in Next.js
+//Will be exchanged for useful information later
+export default async function BlogPost({ params }: BlogPostProps) {
+	
+	//As of Next.js 15 onwards dynamic routing params such as search params should be awaited
+	const postParams = await params;
+	const post = await getPost(postParams.id);
+	const { id, title, body } = post;
+	return (
+		<main className="flex min-h-screen flex-col items-center p-10">
+			<article className="w-full max-w-2xl bg-white shadow-lg rounded-lg overflow-hidden p-6">
+				<Link
+					href="/community-motivation/forums"
+					className="inline-flex items-center text-gray-600 hover:text-gray-900 transition-colors duration-200 mb-6"
+				>
+					Back to all posts
+				</Link>
+				<h1 className="text-3xl md:text-4xl font-extrabold tracking-tight text-gray-900 mb-4">
+					Post {id}: {title}
+				</h1>
+				<p className="text-gray-900">{body}</p>
+			</article>
+		</main>
+	);
+}
