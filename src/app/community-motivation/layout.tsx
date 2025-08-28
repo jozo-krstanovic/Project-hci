482967e5a95addd813d996c5786810956f69423e
jozo-krstanovic <jozo.molten@gmail.com>
2025-08-28 13:43:58 +0200

    Fix import of generic navigation item everywhere

--- src/app/community-motivation/layout.tsx
+++ src/app/community-motivation/layout.tsx
@@ -1,20 +1,20 @@
-import { GenericNavigation, Page } from "@/src/components/genericNavigationBase";
+import { GenericNavigation, Page } from "@/components/genericNavigationBase";
 
 const subPages: Page[] = [
-    { title: "Challenges & leaderboards", path: "/community-motivation/challenges-leaderboards" },
-    { title: "Coach recommendations", path: "/community-motivation/coach-recommendations" },
-    { title: "Forums & support groups", path: "/community-motivation/forums" },
+  { title: "Challenges & leaderboards", path: "/community-motivation/challenges-leaderboards" },
+  { title: "Coach recommendations", path: "/community-motivation/coach-recommendations" },
+  { title: "Forums & support groups", path: "/community-motivation/forums" },
 ];
 
 export default function ShowcaseLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <section className="mt-4">
-        <GenericNavigation pages={subPages} />
-        {children}
-      </section>
-    );
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="mt-4">
+      <GenericNavigation pages={subPages} />
+      {children}
+    </section>
+  );
+}
--- src/app/workout-programs/layout.tsx
+++ src/app/workout-programs/layout.tsx
@@ -1,21 +1,21 @@
-import { GenericNavigation, Page } from "@/src/components/genericNavigationBase";
+import { GenericNavigation, Page } from "@/components/genericNavigationBase";
 
 const subPages: Page[] = [
-    { title: "Quick workouts", path: "/workout-programs/quick-workouts" },
-    { title: "Beginner workouts", path: "/workout-programs/beginner" },
-    { title: "Intermediate & advanced", path: "/workout-programs/intermediate-advanced" },
-    { title: "Flexibility workouts", path: "/workout-programs/low-impact-flexibility" },
+  { title: "Quick workouts", path: "/workout-programs/quick-workouts" },
+  { title: "Beginner workouts", path: "/workout-programs/beginner" },
+  { title: "Intermediate & advanced", path: "/workout-programs/intermediate-advanced" },
+  { title: "Flexibility workouts", path: "/workout-programs/low-impact-flexibility" },
 ];
 
 export default function ShowcaseLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <section className="mt-4">
-        <GenericNavigation pages={subPages} />
-        {children}
-      </section>
-    );
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="mt-4">
+      <GenericNavigation pages={subPages} />
+      {children}
+    </section>
+  );
+}
