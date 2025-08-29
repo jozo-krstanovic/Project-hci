52760cb1032eb6160705eae39b3617430bd7ffd2
jozo-krstanovic <jozo.molten@gmail.com>
2025-08-29 15:31:50 +0200

    allow supabase image domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["xzbtyhqiajfuoldtvkph.supabase.co"],
+  },
 };
 
 export default nextConfig;
