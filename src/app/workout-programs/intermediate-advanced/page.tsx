a12b652f537863fbfc35f69829e48177ab220ee6
jozo-krstanovic <jozo.molten@gmail.com>
2025-09-01 01:01:16 +0200

    Remove unneeded pages (downsizing)

--- src/app/workout-programs/intermediate-advanced/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function IntermediateAdvancedPage() {
-    return (
-      <main className="flex min-h-screen flex-col items-center p-10">
-        <h1 className="text-6xl font-extrabold tracking-tight">Intermediate & Advanced Workouts</h1>
-      </main>
-    );
-  }
